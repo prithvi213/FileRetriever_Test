b90375bca41167cf30728b3efc4131bb530537bc
Prithvi <prithvi.arun27@gmail.com>
2023-06-23 13:49:26 -0700

    Added node tests

--- src/fileretriever.c
+++ src/fileretriever.c
@@ -1,8 +1,11 @@
+#include "node.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-    FILE *f = fopen("../datafiles/load_testretriever.sql", "w+");
+    // Initial code used to test database
+    /*FILE *f = fopen("../datafiles/load_testretriever.sql", "w+");
     fprintf(f, "COPY Files FROM stdin USING DELIMITERS '|';\n");
     char *filename[3] = {"a.text", "b.xls", "c.csv"};
     int filesize[3] = {10, 3, 4};
@@ -12,6 +15,16 @@ int main() {
     }
 
     fprintf(f, "\\.\n");
-    fclose(f);
+    fclose(f);*/
+
+    // Test out node creation and properties
+    Node N = node_create("a", "b", 50, 2, false, "c");
+
+    printf("Node path name: %s\n", getPath(N));
+    printf("Node file name: %s\n", getFileName(N));
+    printf("Node size: %d\n", getSize(N));
+    printf("Node permissions: %d\n", getPermissions(N));
+    printf("Node is directory: %d\n", isDir(N));
+    printf("Node extension: %s\n", getExtension(N));
     return 0;
 }
