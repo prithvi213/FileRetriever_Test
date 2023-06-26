db8bd51a9ad84a74df4e8fd44760b8578b87cbf2
Prithvi <prithvi.arun27@gmail.com>
2023-06-25 18:25:17 -0700

    Created helper function in python file which modifies search path to exact specification for a particular user

--- src/commands.py
+++ src/commands.py
@@ -4,14 +4,29 @@ import sys
 import paramiko
 import psycopg2
 
+def set_search_path(conn, username, pathname):
+    try:
+        cursor = conn.cursor()
+
+        if(username is None or pathname is None):
+            cursor.close()
+            conn.close()
+            return -1
+
+        stmt = "ALTER ROLE %s SET SEARCH_PATH TO %s"
+        cursor.execute(stmt, (username, pathname))
+        cursor.close()
+        return 0
+    except:
+        cursor.close()
+        conn.close()
+        sys.exit(-1)
+
 def main():
     pg_hostname = 'cse182-db.lt.ucsc.edu'
     pg_username = sys.argv[1]
     ucsc_password = sys.argv[2]
     pg_password = sys.argv[3]
-    print(pg_username)
-    print(ucsc_password)
-    print(pg_password)
 
 
     ssh_client = paramiko.SSHClient()
@@ -20,13 +35,11 @@ def main():
     ssh_client.set_missing_host_key_policy(paramiko.WarningPolicy())
     ssh_client.connect('unix.ucsc.edu', username=pg_username, password=ucsc_password)
 
-    myConn = psycopg2.connect(host=pg_hostname, user=pg_username, password=pg_password)
-    cursor = myConn.cursor()
+    conn = psycopg2.connect(host=pg_hostname, user=pg_username, password=pg_password)
 
-    print("SUCCESSFUL CONNECTION")
+    set_search_path(conn, username='parunsha', pathname='FileRetriever')
 
-
-    myConn.close()
+    conn.close()
     ssh_client.close()
 
 if __name__ == '__main__':
