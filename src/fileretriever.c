1eb5ab4978b88b8d822529b93b5b24ac161b9a3a
Prithvi <prithvi.arun27@gmail.com>
2023-07-04 19:27:12 -0700

    Changed the first fprintf line back to its original

--- src/fileretriever.c
+++ src/fileretriever.c
@@ -5,7 +5,7 @@
 
 int main() {
     // Initial code used to test database
-    /*FILE *f = fopen("../datafiles/load_testretriever.sql", "w+");
+    FILE *f = fopen("../datafiles/load_testretriever.sql", "w+");
     fprintf(f, "COPY Files FROM stdin USING DELIMITERS '|';\n");
     char *filename[3] = {"a.text", "b.xls", "c.csv"};
     int filesize[3] = {10, 3, 4};
@@ -15,16 +15,16 @@ int main() {
     }
 
     fprintf(f, "\\.\n");
-    fclose(f);*/
+    fclose(f);
 
     // Test out node creation and properties
-    Node N = node_create("a", "b", 50, 2, false, "c");
+    /*Node N = node_create("a", "b", 50, 2, false, "c");
 
     printf("Node path name: %s\n", getPath(N));
     printf("Node file name: %s\n", getFileName(N));
     printf("Node size: %d\n", getSize(N));
     printf("Node permissions: %d\n", getPermissions(N));
     printf("Node is directory: %d\n", isDir(N));
-    printf("Node extension: %s\n", getExtension(N));
+    printf("Node extension: %s\n", getExtension(N));*/
     return 0;
 }
